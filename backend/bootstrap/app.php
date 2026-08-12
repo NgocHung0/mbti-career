96757e4ad10face4d06f16a1fe813e61284156db
huongzit <huongzit02.nina@gmail.com>
2026-08-12 12:35:19 +0700

    Add standalone Cors middleware class

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -17,23 +17,8 @@ return Application::configure(basePath: dirname(__DIR__))
             return null; // KHÔNG redirect login cho API
         });
 
-        // Tự động đính kèm CORS Headers cho mọi request
-        $middleware->append(function ($request, $next) {
-            if ($request->isMethod('OPTIONS')) {
-                return response('', 200)
-                    ->header('Access-Control-Allow-Origin', '*')
-                    ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-                    ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-            }
-
-            $response = $next($request);
-
-            $response->headers->set('Access-Control-Allow-Origin', '*');
-            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-
-            return $response;
-        });
+        // Đăng ký Class Middleware vừa tạo
+        $middleware->append(\App\Http\Middleware\Cors::class);
 
     })
     ->withExceptions(function (Exceptions $exceptions) {
